744978b471b53e8642879a26abec10308ef10168
OmarAboBakr7 <89665983+OmarAboBakr7@users.noreply.github.com>
2024-05-16 22:36:45 +0300

    Create torad.h

---
+++ Second Milestone/torad.h
@@ -0,0 +1,10 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+float torad(float angle);
