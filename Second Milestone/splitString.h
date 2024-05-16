cf1f59db2edb2f1270ab1c28152ff15ef5ce56e5
OmarAboBakr7 <89665983+OmarAboBakr7@users.noreply.github.com>
2024-05-16 22:26:43 +0300

    Create splitString.h

---
+++ Second Milestone/splitString.h
@@ -0,0 +1,10 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void splitString(char *inputString, char delimiter, char result[15][15]);
