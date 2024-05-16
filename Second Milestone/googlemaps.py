8b87f80a421bb8218f68dde49152d4f120f818e8
FatmaAshraf76 <143643641+FatmaAshraf76@users.noreply.github.com>
2024-05-16 22:22:00 +0300

    Create googlemaps.py

---
+++ Second Milestone/googlemaps.py
@@ -0,0 +1,21 @@
+import gmplot
+import pandas as pd
+import os, sys, string
+
+df = pd.read_csv (r'LOCAT.CSV')
+lat =df['lat'].tolist()
+lon =df['lon'].tolist()
+
+# Create the map plotter:
+gmap = gmplot.GoogleMapPlotter(lat[0], lon[0], 18, apikey="AIzaSyCu-ZEoB8OGYDM3eb31skY43HTRoqdrftA")
+
+gmap.scatter(lat, lon, color='#3B0B39', size=0.2, marker=False)
+gmap.plot(lat,lon,'#ff00f0',edge_width=5)
+
+# Mark a hidden gem:
+gmap.marker(lat[0], lon[0], color='cornflowerblue')
+gmap.marker(lat[-1], lon[-1], color='#ff0000')
+
+# Draw the map:
+gmap.draw('map.html')
+os.startfile("map.html")
