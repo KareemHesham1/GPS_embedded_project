bc668734d38d215c8f353fc9ac7fe2fcfd87ad01
YoussefAbouEl-llel <168706924+YoussefAbouEl-llel@users.noreply.github.com>
2024-05-16 22:26:44 +0300

    Create get_long_lat.c

---
+++ Second Milestone/get_long_lat.c
@@ -0,0 +1,57 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void lat_1 ()
+{
+     if(strcmp(splited[4],"N")==0)
+        {
+             current_lat=atof(splited[3]);
+        }
+        else
+        {
+              current_lat=-1*(atof(splited[3]));
+        }
+
+
+}
+void lat_2 ()
+{
+     if(strcmp(splited[4],"N")==0)
+        {
+             next_lat=atof(splited[3]);
+        }
+        else
+        {
+              next_lat=-1*(atof(splited[3]));
+        }
+
+
+}
+void long_1()
+{
+      if(strcmp(splited[6],"E")==0)
+            {
+             current_long=(atof(splited[5]));
+        }
+        else
+        {
+             current_long=-1*(atof(splited[5]));
+        }
+}
+void long_2()
+{
+      if(strcmp(splited[6],"E")==0)
+            {
+             next_long=(atof(splited[5]));
+        }
+        else
+        {
+             next_long=-1*(atof(splited[5]));
+        }
+}
