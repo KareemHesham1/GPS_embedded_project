7d4d73da61987f3caa2e8aec513cf7a19b8cc6e1
YoussefAbouEl-llel <168706924+YoussefAbouEl-llel@users.noreply.github.com>
2024-05-16 22:29:01 +0300

    Create get_long_lat.h

---
+++ Second Milestone/get_long_lat.h
@@ -0,0 +1,12 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+void lat_1 () ;
+void lat_2 () ;
+void long_1() ;
+void long_2() ;
