7396ce5674467ce3f475d197a5dab31607e707db
MahmoudElleboudy <168694819+MahmoudElleboudy@users.noreply.github.com>
2024-05-16 22:28:13 +0300

    Update UARTS_Utilitiez.h

--- Second Milestone/UARTS_Utilitiez.h
+++ Second Milestone/UARTS_Utilitiez.h
@@ -6,70 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <UARTS_Utilitiez.h>
 
+void UART7_readstr(char* str);
+  
+void UART0_printf(char* str);
 
-
-
-void UART7_readstr(char* str){
-    int i=0;
-    char c;
-
-while(1){
-    int cuntrighr=0;
-        c=UART7_read();
-
-        if(c == '$'){
-            cuntrighr++;
-                    c=UART7_read();
-
-                    if(c== 'G'){
-                        cuntrighr++;
-                        c=UART7_read();
-
-                        if(c == 'P'){
-                            cuntrighr++;
-                            c=UART7_read();
-
-                            if(c == 'R'){
-                                cuntrighr++;
-                                c=UART7_read();
-
-                                if(c == 'M'){
-                                    cuntrighr++;
-                                    c=UART7_read();
-
-                                    if(c == 'C'){
-                                        cuntrighr++;
-                                        while(1){
-
-                                              c=UART7_read();
-
-                                              if(c == 'A'){
-                                                  cuntrighr++;
-                                              }
-
-
-
-        str[i]=c;
-        i++;
-        if (c== '\n' || c == '\r'||c=='*')
-                   {break;}
-                                    }
-                                }
-                            }
-                        }
-                    }
-        }
-    }
-        if(cuntrighr==8)break;
-}
-
-}
-void UART0_printf(char* str){
-
-    while(*str){
-        UART0_write(*str);
-        str++;
-    }
-}
+   
