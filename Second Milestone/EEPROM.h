6c2be3318d5ca8df8c5c6a33b40dc1b47d57a518
KareemHesham1 <168685301+KareemHesham1@users.noreply.github.com>
2024-05-16 22:25:54 +0300

    Update EEPROM.h

--- Second Milestone/EEPROM.h
+++ Second Milestone/EEPROM.h
@@ -1,4 +1,12 @@
 #include "tm4c123gh6pm.h"
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void EEPROMINIT(void);
 void EEPROM_Write(uint32_t address, uint32_t data);
