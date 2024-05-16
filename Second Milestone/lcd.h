2d149aecef890c0336758ebeead50bcfbe2d1141
Jooee2003 <2101115@eng.asu.edu.eg>
2024-05-16 22:29:00 +0300

    Update lcd.h

--- Second Milestone/lcd.h
+++ Second Milestone/lcd.h
@@ -6,3 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
+void delay(long d);
+void LCD_CMD(uint8_t cmd);
+void LCD_Init(void);
+void LCD_SendChar(uint8_t data);
+void LCD_SendString(uint8_t *str ,uint8_t len);
